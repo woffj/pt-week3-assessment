e0f5c7dc5a4650fd67b1117e68ccbd1ee4981f89
Tyler McGinnis <tylermcginnis33@gmail.com>
2014-10-04 07:56:02 -0600

    Add Files

---
+++ callbacks.js
@@ -0,0 +1,8 @@
+/* 
+  Write a function called reverse that makes the code below work properly.
+  The code below should log "The reversedString is .desrever teg dluohs sihT"
+*/
+var sentence = 'This should get reversed.'
+reverse(sentence, function(reversedString){
+  console.log('The reversedString is ' + reversedString);
+});
---
+++ constructor.js
@@ -0,0 +1,28 @@
+/*Create a function called 'person' that accepts a name and an age parameter
+  and returns an object with those properties. 
+*/
+
+    //code here
+
+/* Call your function twice creating to different objects
+
+
+    //code here
+
+/* In this scenario, can you add a method to your person functions prototoype and then have
+   the objects you create with that function delegate to the person's prototype?
+
+   //Explain your answer here
+   /*  ....  */
+
+
+/* Create a Constructor function called 'Person' that accepts a name and an age paramter
+    and returns an object with those parameters */
+
+    //code here
+
+/* Add a property called sayAge to the Person's prototype which says that instances name */
+
+    //code here
+
+/* Create two instances of Person and call sayAge with one of those instances.
