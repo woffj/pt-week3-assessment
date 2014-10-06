64167ed947bb3e4e10eeadef19900edc5642a8df
Jeimi Woffinden <jeimi@yahoo.com>
2014-10-05 21:12:47 -0600

    week3 assessment

--- callbacks.js
+++ callbacks.js
@@ -2,7 +2,16 @@
   Write a function called reverse that makes the code below work properly.
   The code below should log "The reversedString is .desrever teg dluohs sihT"
 */
+
+var reverse = function(str, cb) {
+	var reversedString = str.split('').reverse('').join('');
+	cb(str);
+ };
+
+
 var sentence = 'This should get reversed.'
 reverse(sentence, function(reversedString){
   console.log('The reversedString is ' + reversedString);
 });
+
+
--- constructor.js
+++ constructor.js
@@ -1,25 +1,45 @@
 /*Create a function called 'person' that accepts a name and an age parameter
   and returns an object with those properties. 
 */
+	// function
+	var person = function(name, age) {
+		return {
+			name: name,
+			age: age
+		}
+	}
 
-    //code here
+	// constructor function
+    var Person = function(name, age) {
+    	this.name = name;
+    	this.age = age;
+    }
 
-/* Call your function twice creating to different objects
 
+// Call your function twice creating to different objects
+	// function 
+	var jeimi = person('Jeimi', 39);
 
-    //code here
+	// constructor
+    var jeimi = new Person('Jeimi', 39);
+    var aaron = new Person('Aaron', 33);
 
 /* In this scenario, can you add a method to your person functions prototoype and then have
-   the objects you create with that function delegate to the person's prototype?
+   the objects you create with that function delegate to the person's prototype? */
 
-   //Explain your answer here
-   /*  ....  */
-
+   Person.prototype.hairColor = function() {
+   	this.hairColor = hairColor;
+   }
+ 	Person.prototype.eyeColor = function() {
+ 	this.eyeColor = eyeColor;
+   }
+   jeimi.hairColor = 'brown';
 
 /* Create a Constructor function called 'Person' that accepts a name and an age paramter
     and returns an object with those parameters */
 
-    //code here
+
+
 
 /* Add a property called sayAge to the Person's prototype which says that instances name */
 
